c5593b60053ec8663a8a38a5a3005ff0b8ad6cd6
Antti Samuli <antti.samuli@fmi.fi>
2021-11-04 16:41:01 +0200

    kurssitiedot step3

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -2,12 +2,18 @@ import React from 'react'
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   const Header = (props) => {
     return (
@@ -20,21 +26,23 @@ const App = () => {
   }
 
   const Part = (props) => {
+    console.log("parti", props)
     return (
       <>
         <p>
-        {props.par} {props.exer}
+        {props.par.name} {props.par.exercises}
         </p>
       </>
     )
   }
 
   const Content = (props) => {
+    console.log("conti", props)
     return (
       <>
-        <Part par={props.part1} exer={props.exercises1}/>
-        <Part par={props.part2} exer={props.exercises2}/>
-        <Part par={props.part3} exer={props.exercises3}/>
+        <Part par={props.part1}/>
+        <Part par={props.part2}/>
+        <Part par={props.part3}/>
       </>
     )
   }
@@ -52,29 +60,13 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} exercises1={exercises1}
-               part2={part2} exercises2={exercises2}
-               part3={part3} exercises3={exercises3} />
-      <Total exercises={exercises1 + exercises2 + exercises3}/>
+      <Content part1={part1} 
+               part2={part2} 
+               part3={part3}  />
+      <Total exercises={part1.exercises + part2.exercises + part3.exercises}/>
     </div>
   )
-
-
-  // return (
-  //   <div>
-  //     <h1>{course}</h1>
-  //     <p>
-  //       {part1} {exercises1}
-  //     </p>
-  //     <p>
-  //       {part2} {exercises2}
-  //     </p>
-  //     <p>
-  //       {part3} {exercises3}
-  //     </p>
-  //     <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-  //   </div>
-  // )
 }
+
 
 export default App
