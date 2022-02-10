336841a027db2c49640aa17b353cd70e9e53f2bb
Antti Samuli <antti.samuli@fmi.fi>
2022-02-10 16:41:13 +0200

    1.13 anekdootit step2

--- osa1/anekdootit/src/App.js
+++ osa1/anekdootit/src/App.js
@@ -10,16 +10,28 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.'
   ]
-   
-  const [selected, setSelected] = useState(0)
 
+  // const points = Array(7).fill(0);
+  const points = Array(anecdotes.length).fill(0);
+
+  const [selected, setSelected] = useState(0)
+  const [statistics, setStatistics] = useState(points)
+  
   const handleSelect = () => {
     setSelected(Math.floor(Math.random() * 7))
+  }
+
+  const handleVote = () => {
+    const newStat = [...statistics]
+    newStat[selected] +=1
+    setStatistics(newStat)
   }
 
   return (
     <div>
       <p>{selected} - {anecdotes[selected]}</p>
+      <p>has {statistics[selected]} votes</p>
+      <button onClick={handleVote}>vote</button>
       <button onClick={handleSelect}>next anecdote</button>
     </div>
   )
